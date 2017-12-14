b198d8417decadc6b4a984b9dd996ce4698571f4
Ozzie Gooen <Ozzie@grandroundshealth.com>
2017-12-14 15:44:24 -0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import ApolloClient, { createNetworkInterface } from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import { createStore, combineReducers } from "redux";
+
+const { SERVER_URL } = process.env;
+// const networkInterface = createNetworkInterface({ uri: process.env.REACT_APP_SERVER_URL });
+const networkInterface = createNetworkInterface({ uri: "localhost:3000" });
+const reduxDevtoolsMiddleware: any =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__();
+
+const client = new ApolloClient({
+  networkInterface,
+  dataIdFromObject: (o: { id: string }) => o.id,
+});
+
+const store = createStore(
+  reduxDevtoolsMiddleware
+);
+
+export class Layout extends React.Component {
+  public render() {
+    return (
+      <div className="app-container">
+        <div className="app-content">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+const LandingPage = () => (
+  <div>
+    hi there!
+  </div>
+);
+
+const Routes = () => (
+  <div>
+    <Route exact={true} path="/" component={LandingPage} />
+  </div>
+);
+
+class App extends React.Component {
+  public render() {
+    return (
+      <ApolloProvider store={store} client={client}>
+        <BrowserRouter>
+          <Layout>
+            <Routes />
+          </Layout>
+        </BrowserRouter>
+      </ApolloProvider>
+    );
+  }
+}
+
+export { App };
