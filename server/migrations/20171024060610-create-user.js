ab6bd3b2f668ee84ccc945b848510d5087d9c16d
Ozzie Gooen <Ozzie@grandroundshealth.com>
2017-12-14 22:57:37 -0800

    Very basic database connection working

---
+++ server/migrations/20171024060610-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      confirmationToken: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      hasConfirmedEmail: {
+        type: Sequelize.BOOLEAN
+      },
+      ethereumAddress: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
